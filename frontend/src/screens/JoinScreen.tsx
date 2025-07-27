17b1033a3e657468bb9fae5a80c52d350a8de074
Florian Gräve <floriangraeve@gmail.com>
2025-07-27 17:24:10 +0200

    Test Commit für Deployment

--- frontend/src/screens/JoinScreen.tsx
+++ frontend/src/screens/JoinScreen.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const JoinScreen = () => (
   <div className="p-4">
-    <h2 className="text-xl font-bold text-yellow-700">Umfrage beitreten</h2>
+    <h2 className="text-xl font-bold text-yellow-700">Umfrage beitreten </h2>
     <p>Hier kannst du einer Umfrage beitreten.</p>
   </div>
 )
