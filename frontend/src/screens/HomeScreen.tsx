457a6c566074fb1203ebbd72822663e87dbecca2
Pascal <hypstc@gmail.com>
2025-07-29 20:33:35 +0200

    cleanup

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import ActionButton from "./Components/Buttons";
+import ActionButton from "./Components/ActionButton";
 
 const HomeScreen = () => {
   const navigate = useNavigate();
